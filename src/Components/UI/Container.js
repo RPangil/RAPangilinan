fe9b681db7bcf7260ca49d8c0512bf53fc978bf2
RPangil <rapangilinan12@gmail.com>
2023-05-22 16:12:18 -0400

    Containers accept classes

--- src/Components/UI/Container.js
+++ src/Components/UI/Container.js
@@ -1,8 +1,10 @@
+import styles from '../../SCSS/main.module.scss';
+
 function Container(props)
 {
     return(
         <section className={props.className}>
-            <div className="container">
+            <div className={styles.container}>
                 {props.title ? <h2>{props.title}</h2> : ''}
                 {props.children}
             </div>
