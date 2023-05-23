9ea064cbf75cc3f2466d245ff2fc585747718811
RPangil <rapangilinan12@gmail.com>
2023-05-23 17:35:03 -0400

    Update Slider.js

--- src/Components/UI/Slider/Slider.js
+++ src/Components/UI/Slider/Slider.js
@@ -1,9 +1,16 @@
 import Slide from "./Slide/Slide";
 import SlideNav from "./SlideNav/SlideNav";
 
+import style from "./Slider.module.scss";
+
 function Slider(props)
 {
-    
+    return (
+        <div className={style.container}>
+            <SlideNav></SlideNav>
+            <Slide></Slide>
+        </div>
+    );
 }
 
 export default Slider;
