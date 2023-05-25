f4f93382d30373c827c617afe730022d4f1678e8
RPangil <rapangilinan12@gmail.com>
2023-05-25 14:37:14 -0400

    Added base files for nav

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,16 @@
+import styles from './Header.module.scss';
+
 function Header()
 {
-
+    return (
+        <header>
+            <nav>
+                <div className={styles.logo}></div>
+                <div className={styles.nav}></div>
+                
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
