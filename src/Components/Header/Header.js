66e021b6c79ece0d2fcedebd7b712408b8f706d3
RPangil <rapangilinan12@gmail.com>
2023-05-25 19:15:21 -0400

    Update Header.js
    Added links to scroll to sections

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-scroll';
+
 import styles from './Header.module.scss';
 
 function Header()
@@ -6,8 +8,45 @@ function Header()
         <header>
             <nav>
                 <div className={styles.logo}></div>
-                <div className={styles.nav}></div>
-                
+                <div className={styles.nav}>
+                    <Link
+                        to='About'
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        isDynamic={true}
+                    >
+                        About
+                    </Link>
+                    <Link
+                        to='Skills'
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        isDynamic={true}
+                    >
+                        Skills
+                    </Link>
+                    <Link
+                        to='Experience'
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        isDynamic={true}
+                    >
+                        Experience
+                    </Link>
+                    <Link
+                        to='Work'
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        isDynamic={true}
+                    >
+                        Work
+                    </Link>
+                </div>
+                <div className={styles.menu}></div>
             </nav>
         </header>
     );
