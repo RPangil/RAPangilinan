92e966e98e63f2082f0b6ab20f0f0d447ea22a43
RPangil <rapangilinan12@gmail.com>
2023-05-25 20:59:12 -0400

    Added mobile menu
    Styled header and added on click function

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,24 @@
 import { Link } from 'react-scroll';
 
 import styles from './Header.module.scss';
+import { useState } from 'react';
 
 function Header()
 {
+    const [menuClick, setMenuClick] = useState(false);
+
+    function menuIsClicked()
+    {
+        if(menuClick === false)
+        {
+            setMenuClick(true);
+        }
+        else
+        {
+            setMenuClick(false);
+        }
+    }
+
     return (
         <header>
             <nav>
@@ -57,11 +72,51 @@ function Header()
                     </Link>
                 </div>
                 <div className={styles.menu}>
-                    <button>
+                    <button aria-label='Menu' onClick={menuIsClicked}>
                         <i></i>
                         <i></i>
                         <i></i>
                     </button>
+                    <div className={`${styles.menuNav} ${menuClick ? styles.open : ''}`}>
+                        <nav>
+                            <Link
+                                to='About'
+                                spy={true}
+                                smooth={true}
+                                duration={500}
+                                isDynamic={true}
+                            >
+                                About
+                            </Link>
+                            <Link
+                                to='Skills'
+                                spy={true}
+                                smooth={true}
+                                duration={500}
+                                isDynamic={true}
+                            >
+                                Skills
+                            </Link>
+                            <Link
+                                to='Experience'
+                                spy={true}
+                                smooth={true}
+                                duration={500}
+                                isDynamic={true}
+                            >
+                                Experience
+                            </Link>
+                            <Link
+                                to='Work'
+                                spy={true}
+                                smooth={true}
+                                duration={500}
+                                isDynamic={true}
+                            >
+                                Work
+                            </Link>
+                        </nav>
+                    </div>
                 </div>
             </nav>
         </header>
