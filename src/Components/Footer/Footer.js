7df5261f6530861b42af83c329fb7b4ac2292b65
RPangil <rapangilinan12@gmail.com>
2023-05-26 16:41:24 -0400

    Added github and linkedin links
    Added the logos for github and linkedin. Also added their responsive styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,10 @@ function Footer()
     return (
         <footer>
             <div className={styles.container}>
+                <div className={styles.link}>
+                    <a href='https://www.linkedin.com/in/rapangilinan12/' target='_blank' rel='noreferrer' className={styles.linkedIn}></a>
+                    <a href='https://github.com/RPangil' target='_blank' rel='noreferrer' className={styles.github}></a>
+                </div>
                 <p>Designed &amp; Built by Robert Pangilinan &copy;2023</p>
             </div>
         </footer>
