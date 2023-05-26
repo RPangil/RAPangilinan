26f9a74aaee271ad1b8db91b2dca6793fd90bdf5
RPangil <rapangilinan12@gmail.com>
2023-05-25 22:44:23 -0400

    Added footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,14 @@
+import styles from './Footer.module.scss';
+
 function Footer()
 {
-    
+    return (
+        <footer>
+            <div className={styles.container}>
+                <p>Designed &amp; Built by Robert Pangilinan &copy;2023</p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
