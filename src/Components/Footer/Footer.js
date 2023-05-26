11e45c1ab46b259b3ba403888cfab2090922d47c
RPangil <rapangilinan12@gmail.com>
2023-05-26 17:23:02 -0400

    Added transparent text
    Added text to enable screen reading but made text transparent to leave icons visible

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,8 +6,8 @@ function Footer()
         <footer>
             <div className={styles.container}>
                 <div className={styles.link}>
-                    <a href='https://www.linkedin.com/in/rapangilinan12/' target='_blank' rel='noreferrer' className={styles.linkedIn}></a>
-                    <a href='https://github.com/RPangil' target='_blank' rel='noreferrer' className={styles.github}></a>
+                    <a href='https://www.linkedin.com/in/rapangilinan12/' target='_blank' rel='noreferrer' className={styles.linkedIn}>LinkedIn</a>
+                    <a href='https://github.com/RPangil' target='_blank' rel='noreferrer' className={styles.github}>Github</a>
                 </div>
                 <p>Designed &amp; Built by Robert Pangilinan &copy;2023</p>
             </div>
